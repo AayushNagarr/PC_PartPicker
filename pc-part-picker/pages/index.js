5475ed5df1eb1ae8e9514a19e19108903c17df8e
AayushNagarr <92391624+AayushNagarr@users.noreply.github.com>
2023-11-08 21:20:44 +0530

    Login Page

--- pc-part-picker/pages/index.js
+++ pc-part-picker/pages/index.js
@@ -1,10 +1,58 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from
+ 
+'next/image';
+import { Inter } from
+ 
+'next/font/google';
+import { useState } from
+ 
+'react';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
-export default function Home() {
+
+
+export default function
+ 
+Home() {
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleSubmit = () => {
+    if (username === 'admin' && password === 'admin') {
+      alert('Login Successful');
+      setIsLoggedIn(true);
+    } else {
+      alert('Invalid username or password');
+    }
+  }
+
   return (
-   
-  )
+    <div className="grid h-screen place-items-center border-2 border-sky-500">
+      <div className="flex flex-col items-center border-2 border-green-400 w-1/2 h-1/2">
+        <h1 className="font-bold text-4xl">Login</h1>
+        <label className="font-bold text-2xl">Username</label>
+        <input
+          onChange={(e) => setUsername(e.target.value)}
+          value={username}
+          className="border-2 border-green-400 text-black"
+          type="text"
+        />
+        <label className="font-bold text-2xl">Password</label>
+        <input
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          className="border-2 border-green-400 text-black"
+          type="password"
+        />
+        <button onClick={handleSubmit} className="border-2 border-green-400">
+          Login
+        </button>
+      </div>
+
+      {isLoggedIn && <div>Hello World!</div>}
+    </div>
+  );
 }
