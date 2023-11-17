bdff0696083376e24e1f898e202ddd48ab6acd40
AayushNagarr <92391624+AayushNagarr@users.noreply.github.com>
2023-11-17 11:32:11 +0530

    user checkout

--- pc-part-picker/app/api/checkout/route.js
+++ pc-part-picker/app/api/checkout/route.js
@@ -7,7 +7,7 @@ export async function POST(request) {
     const { user } = await request.json();
     console.log("In checkout route", user);
     const client = await pool.connect();
-    const insertQuery = `SELECT SUM(price) AS total_price FROM carts`;
+    const insertQuery = `SELECT SUM(price) AS total_price FROM carts JOIN users on users.id = carts.user_id WHERE users.username = '${user}'`;
     const  result  = await client.query(insertQuery);
     const items = result.rows[0];
     console.log("checkout api result", result.rows[0]);
